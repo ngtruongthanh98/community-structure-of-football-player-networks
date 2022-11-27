089365419b8f456f5257547089a5d4293ba1b5f7
ngtruongthanh98 <ngtruongthanh98@gmail.com>
2022-11-27 23:15:50 +0700

    feat: ScatterChart component

---
+++ frontend/src/components/Charts/ScatterChart/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {
+  Chart as ChartJS,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Scatter } from 'react-chartjs-2';
+import faker from 'faker';
+
+ChartJS.register(LinearScale, PointElement, LineElement, Tooltip, Legend);
+const ScatterChart = (props) => {
+  const options = {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  };
+
+  const data = {
+    datasets: [
+      {
+        label: 'A dataset',
+        data: Array.from({ length: 100 }, () => ({
+          x: faker.datatype.number({ min: -100, max: 100 }),
+          y: faker.datatype.number({ min: -100, max: 100 }),
+        })),
+        backgroundColor: 'rgba(255, 99, 132, 1)',
+      },
+    ],
+  };
+
+  return <Scatter options={options} data={data} {...props} />;
+};
+
+export default ScatterChart;
