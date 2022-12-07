c2117242323cf5ff4d96d20711b4a33d8b741694
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-12-08 00:34:38 +0700

    feat: Handle data add player attributes

--- frontend/src/pages/Management/index.js
+++ frontend/src/pages/Management/index.js
@@ -1,22 +1,47 @@
 import React, { useState } from 'react';
 import './styles.scss';
 import { Input, Button, Image } from '@nextui-org/react';
+import _ from 'lodash';
 
 const Management = () => {
   const [playerName, setPlayerName] = useState('');
   const [playerPositions, setPlayerPositions] = useState('');
   const [weight, setWeight] = useState(0);
   const [height, setHeight] = useState(0);
-  const [statsArray, setStatsArray] = useState([]);
+  const [yearOfBirth, setYearOfBirth] = useState(0);
+
+  const [playerAttributes, setPlayerAttributes] = useState([]);
+  const [playerAttributeValues, setPlayerAttributeValues] = useState([]);
 
   const handleSubmit = () => {
-    console.log('Player name: ', playerName);
-    console.log('Player positions: ', playerPositions);
+    const positions = playerPositions.split(',');
+    const attributes = playerAttributes.split(',');
+    const attributeValues = playerAttributeValues.split(',');
+
+    if (attributes.length === 0 || attributeValues.length === 0) {
+      return;
+    }
+
+    const attributeObject = {};
+
+    _.forEach(attributes, (attribute, index) => {
+      attributeObject[attribute] = attributeValues[index];
+    });
+
+    const playerData = {
+      name: playerName,
+      positions: positions,
+      weight: weight,
+      height: height,
+      yearOfBirth: yearOfBirth,
+      attributes: attributeObject,
+    };
+
+    console.log('playerData: ', playerData);
   };
 
   return (
     <div className="management-page">
-      {/* Add new player */}
       <div className="add-player-container">
         <div className="title">Add new player</div>
 
@@ -51,6 +76,17 @@ const Management = () => {
               className="next-input"
               width="300px"
             />
+
+            <Input
+              clearable
+              label="Year of birth"
+              placeholder="Enter player year of birth"
+              bordered
+              color="primary"
+              onChange={(e) => setYearOfBirth(e.target.value)}
+              className="next-input"
+              width="300px"
+            />
           </div>
 
           <div className="input-container__column">
@@ -72,6 +108,30 @@ const Management = () => {
               bordered
               color="primary"
               onChange={(e) => setHeight(e.target.value)}
+              className="next-input"
+              width="300px"
+            />
+
+            <Input
+              clearable
+              label="Attributes"
+              placeholder="Enter attributes"
+              bordered
+              color="primary"
+              helperText="Enter attributes separated by comma"
+              onChange={(e) => setPlayerAttributes(e.target.value)}
+              className="next-input"
+              width="300px"
+            />
+
+            <Input
+              clearable
+              label="Attribute values"
+              placeholder="Enter attribute values"
+              bordered
+              color="primary"
+              helperText="Enter attribute values separated by comma"
+              onChange={(e) => setPlayerAttributeValues(e.target.value)}
               className="next-input"
               width="300px"
             />
