521af59a63b7957ca774e60a3584dab16f4b38b3
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-12-09 19:12:50 +0700

    refactor: Refactor management page

---
+++ frontend/src/pages/Management/DeletePlayer/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const DeletePlayer = () => {
+  return (
+    <div className="player-container">
+      <div className="title">Delete a player</div>
+    </div>
+  );
+};
+
+export default DeletePlayer;
