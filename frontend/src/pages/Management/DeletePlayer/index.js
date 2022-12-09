2dbbd555ef4fc7cc597ac6d380172aa8734c06c0
ThanhNT26 <ngtruongthanh98@gmail.com>
2022-12-09 20:00:00 +0700

    feat; Delete player view

--- frontend/src/pages/Management/DeletePlayer/index.js
+++ frontend/src/pages/Management/DeletePlayer/index.js
@@ -1,9 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
+import HomePage from '../../../pages/Home';
+import { Input, Button } from '@nextui-org/react';
+import './styles.scss';
 
 const DeletePlayer = () => {
+  const [playerId, setPlayerId] = useState('');
+  const onHandleDelete = () => {};
+
   return (
     <div className="player-container">
       <div className="title">Delete a player</div>
+
+      <div className="controller-container">
+        <Input
+          clearable
+          label="Player ID"
+          placeholder="Enter player id"
+          bordered
+          color="primary"
+          onChange={(e) => {
+            setPlayerId(e.target.value);
+          }}
+          className="next-input"
+          width="300px"
+        />
+
+        <Button auto color="gradient" rounded className="delete-btn" onClick={onHandleDelete}>
+          Delete player
+        </Button>
+      </div>
+
+      {playerId}
+
+      <HomePage />
     </div>
   );
 };
